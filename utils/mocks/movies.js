9cb105f8bdb07298f36734c7b5524629809d3dd7
= <stephen.catano@icloud.com>
2019-11-06 17:00:21 -0500

    Ajustando Rutas y Utilidades

---
+++ utils/mocks/movies.js
@@ -0,0 +1,155 @@
+const moviesMock = [
+  {
+    id: '3375ecfd-7c3a-48dd-b22e-bb536ac88a2c',
+    title: 'Wholly Moses',
+    year: 1992,
+    cover: 'http://dummyimage.com/206x113.png/dddddd/000000',
+    description:
+      'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.',
+    duration: 91,
+    contentRating: 'R',
+    source:
+      'https://admin.ch/libero/convallis.xml?phasellus=eget&sit=nunc&amet=donec&erat=quis&nulla=orci&tempus=eget&vivamus=orci&in=vehicula&felis=condimentum&eu=curabitur&sapien=in&cursus=libero&vestibulum=ut&proin=massa&eu=volutpat&mi=convallis&nulla=morbi&ac=odio&enim=odio&in=elementum&tempor=eu&turpis=interdum&nec=eu&euismod=tincidunt&scelerisque=in&quam=leo&turpis=maecenas&adipiscing=pulvinar&lorem=lobortis&vitae=est&mattis=phasellus&nibh=sit&ligula=amet&nec=erat&sem=nulla&duis=tempus&aliquam=vivamus&convallis=in&nunc=felis&proin=eu&at=sapien&turpis=cursus&a=vestibulum&pede=proin&posuere=eu&nonummy=mi&integer=nulla&non=ac&velit=enim&donec=in&diam=tempor&neque=turpis&vestibulum=nec&eget=euismod&vulputate=scelerisque&ut=quam&ultrices=turpis&vel=adipiscing&augue=lorem&vestibulum=vitae&ante=mattis&ipsum=nibh&primis=ligula&in=nec&faucibus=sem&orci=duis&luctus=aliquam&et=convallis&ultrices=nunc&posuere=proin&cubilia=at&curae=turpis&donec=a&pharetra=pede&magna=posuere&vestibulum=nonummy&aliquet=integer&ultrices=non',
+    tags: ['Action']
+  },
+  {
+    id: '368d7208-570c-4ad7-bd7e-beb53885aec0',
+    title: 'After Death (Posle smerti)',
+    year: 2005,
+    cover: 'http://dummyimage.com/220x234.jpg/cc0000/ffffff',
+    description:
+      'Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.',
+    duration: 15,
+    contentRating: 'PG',
+    source:
+      'https://dedecms.com/proin/at/turpis/a/pede.json?dolor=lobortis&vel=ligula&est=sit&donec=amet&odio=eleifend&justo=pede&sollicitudin=libero&ut=quis&suscipit=orci&a=nullam&feugiat=molestie&et=nibh&eros=in&vestibulum=lectus&ac=pellentesque&est=at&lacinia=nulla&nisi=suspendisse&venenatis=potenti&tristique=cras&fusce=in&congue=purus&diam=eu&id=magna&ornare=vulputate&imperdiet=luctus&sapien=cum&urna=sociis&pretium=natoque&nisl=penatibus&ut=et&volutpat=magnis&sapien=dis&arcu=parturient&sed=montes&augue=nascetur&aliquam=ridiculus&erat=mus&volutpat=vivamus&in=vestibulum&congue=sagittis&etiam=sapien&justo=cum&etiam=sociis&pretium=natoque&iaculis=penatibus&justo=et&in=magnis&hac=dis&habitasse=parturient&platea=montes&dictumst=nascetur&etiam=ridiculus&faucibus=mus&cursus=etiam&urna=vel&ut=augue&tellus=vestibulum&nulla=rutrum&ut=rutrum&erat=neque&id=aenean&mauris=auctor&vulputate=gravida&elementum=sem&nullam=praesent&varius=id&nulla=massa&facilisi=id&cras=nisl&non=venenatis&velit=lacinia&nec=aenean&nisi=sit&vulputate=amet&nonummy=justo&maecenas=morbi&tincidunt=ut&lacus=odio&at=cras&velit=mi&vivamus=pede',
+    tags: [
+      'Drama|Thriller',
+      'Action|Sci-Fi',
+      'Comedy|Horror|Thriller',
+      'Drama',
+      'Comedy'
+    ]
+  },
+  {
+    id: '45a003ee-cfb1-4ce3-abef-142e1ddb28e1',
+    title: 'A Promise',
+    year: 2007,
+    cover: 'http://dummyimage.com/204x161.bmp/5fa2dd/ffffff',
+    description:
+      'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.',
+    duration: 37,
+    contentRating: 'PG',
+    source:
+      'http://wiley.com/in.aspx?neque=blandit&sapien=lacinia&placerat=erat&ante=vestibulum&nulla=sed&justo=magna&aliquam=at&quis=nunc&turpis=commodo&eget=placerat&elit=praesent&sodales=blandit&scelerisque=nam&mauris=nulla&sit=integer&amet=pede&eros=justo&suspendisse=lacinia&accumsan=eget&tortor=tincidunt&quis=eget&turpis=tempus&sed=vel&ante=pede&vivamus=morbi&tortor=porttitor&duis=lorem&mattis=id&egestas=ligula&metus=suspendisse&aenean=ornare&fermentum=consequat&donec=lectus&ut=in&mauris=est&eget=risus&massa=auctor&tempor=sed&convallis=tristique&nulla=in&neque=tempus&libero=sit&convallis=amet&eget=sem&eleifend=fusce&luctus=consequat&ultricies=nulla&eu=nisl&nibh=nunc&quisque=nisl&id=duis&justo=bibendum&sit=felis&amet=sed&sapien=interdum&dignissim=venenatis&vestibulum=turpis&vestibulum=enim&ante=blandit&ipsum=mi&primis=in&in=porttitor&faucibus=pede&orci=justo&luctus=eu&et=massa&ultrices=donec&posuere=dapibus&cubilia=duis&curae=at&nulla=velit&dapibus=eu&dolor=est&vel=congue&est=elementum&donec=in&odio=hac&justo=habitasse&sollicitudin=platea&ut=dictumst&suscipit=morbi&a=vestibulum&feugiat=velit&et=id&eros=pretium&vestibulum=iaculis&ac=diam&est=erat&lacinia=fermentum&nisi=justo&venenatis=nec',
+    tags: ['Drama', 'Action|Crime|Film-Noir', 'Adventure|Drama|Romance']
+  },
+  {
+    id: '63a54d0f-918e-4c94-9a66-3b49df1eeca2',
+    title: 'Slap Shot 2: Breaking the Ice',
+    year: 1992,
+    cover: 'http://dummyimage.com/187x243.bmp/dddddd/000000',
+    description:
+      'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.',
+    duration: 77,
+    contentRating: 'PG',
+    source:
+      'http://who.int/ultrices/posuere.xml?a=suscipit&libero=ligula&nam=in&dui=lacus&proin=curabitur&leo=at&odio=ipsum&porttitor=ac&id=tellus&consequat=semper&in=interdum&consequat=mauris&ut=ullamcorper&nulla=purus&sed=sit',
+    tags: [
+      'Comedy',
+      'Horror|Mystery|Thriller',
+      'Comedy',
+      'Documentary',
+      'Comedy'
+    ]
+  },
+  {
+    id: 'e55895ce-d1b6-4a5d-a344-bda94cb78bd7',
+    title: 'Batman/Superman Movie, The',
+    year: 2005,
+    cover: 'http://dummyimage.com/111x192.png/ff4444/ffffff',
+    description:
+      'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.',
+    duration: 22,
+    contentRating: 'PG',
+    source:
+      'https://hao123.com/eget/orci/vehicula/condimentum/curabitur/in.jsp?orci=mauris&nullam=sit&molestie=amet&nibh=eros&in=suspendisse&lectus=accumsan&pellentesque=tortor&at=quis&nulla=turpis&suspendisse=sed&potenti=ante&cras=vivamus&in=tortor&purus=duis&eu=mattis&magna=egestas&vulputate=metus&luctus=aenean&cum=fermentum&sociis=donec&natoque=ut&penatibus=mauris&et=eget&magnis=massa&dis=tempor&parturient=convallis&montes=nulla&nascetur=neque&ridiculus=libero&mus=convallis&vivamus=eget&vestibulum=eleifend&sagittis=luctus&sapien=ultricies&cum=eu&sociis=nibh&natoque=quisque&penatibus=id&et=justo&magnis=sit&dis=amet&parturient=sapien&montes=dignissim&nascetur=vestibulum&ridiculus=vestibulum&mus=ante&etiam=ipsum&vel=primis&augue=in&vestibulum=faucibus&rutrum=orci&rutrum=luctus&neque=et&aenean=ultrices&auctor=posuere&gravida=cubilia&sem=curae&praesent=nulla&id=dapibus&massa=dolor&id=vel&nisl=est&venenatis=donec&lacinia=odio&aenean=justo&sit=sollicitudin',
+    tags: [
+      'Drama',
+      'Action|Adventure|Comedy|Western',
+      'Drama|Romance',
+      'Crime|Drama|Musical|Thriller'
+    ]
+  },
+  {
+    id: 'ea7c4e11-1290-4041-91e5-f66390304328',
+    title:
+      'Dragon Ball: The Curse Of The Blood Rubies (Doragon bôru: Shenron no densetsu)',
+    year: 1995,
+    cover: 'http://dummyimage.com/134x177.jpg/ff4444/ffffff',
+    description:
+      'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.',
+    duration: 82,
+    contentRating: 'G',
+    source:
+      'http://i2i.jp/nec/nisi/vulputate/nonummy/maecenas.png?non=semper&velit=interdum&nec=mauris&nisi=ullamcorper&vulputate=purus&nonummy=sit&maecenas=amet&tincidunt=nulla&lacus=quisque&at=arcu&velit=libero&vivamus=rutrum&vel=ac',
+    tags: ['Documentary']
+  },
+  {
+    id: '87c8e531-a13f-4817-8a59-3deb4ab83d27',
+    title: 'Vivere',
+    year: 1997,
+    cover: 'http://dummyimage.com/203x124.jpg/5fa2dd/ffffff',
+    description:
+      'Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    duration: 7,
+    contentRating: 'R',
+    source:
+      'https://bloglovin.com/nam/ultrices/libero.jsp?sit=a&amet=libero&consectetuer=nam&adipiscing=dui&elit=proin&proin=leo&risus=odio&praesent=porttitor&lectus=id&vestibulum=consequat&quam=in&sapien=consequat&varius=ut&ut=nulla&blandit=sed&non=accumsan&interdum=felis&in=ut&ante=at&vestibulum=dolor&ante=quis&ipsum=odio&primis=consequat&in=varius&faucibus=integer&orci=ac&luctus=leo&et=pellentesque&ultrices=ultrices&posuere=mattis&cubilia=odio&curae=donec&duis=vitae&faucibus=nisi&accumsan=nam&odio=ultrices&curabitur=libero&convallis=non&duis=mattis&consequat=pulvinar&dui=nulla&nec=pede&nisi=ullamcorper&volutpat=augue&eleifend=a&donec=suscipit&ut=nulla&dolor=elit&morbi=ac&vel=nulla&lectus=sed&in=vel&quam=enim&fringilla=sit&rhoncus=amet&mauris=nunc&enim=viverra&leo=dapibus&rhoncus=nulla&sed=suscipit&vestibulum=ligula&sit=in&amet=lacus&cursus=curabitur&id=at',
+    tags: ['Comedy', 'Comedy']
+  },
+  {
+    id: '38977eb7-d017-4a8c-bc3b-83251629cf06',
+    title: 'Meet the Feebles',
+    year: 2008,
+    cover: 'http://dummyimage.com/101x202.png/cc0000/ffffff',
+    description:
+      'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.',
+    duration: 47,
+    contentRating: 'PG',
+    source:
+      'http://telegraph.co.uk/nulla/sed/accumsan/felis/ut/at/dolor.html?sagittis=nisl&sapien=ut&cum=volutpat&sociis=sapien&natoque=arcu&penatibus=sed&et=augue&magnis=aliquam&dis=erat&parturient=volutpat&montes=in&nascetur=congue&ridiculus=etiam&mus=justo&etiam=etiam&vel=pretium&augue=iaculis&vestibulum=justo&rutrum=in',
+    tags: ['Drama', 'Drama|Romance', 'Adventure|Fantasy|Sci-Fi']
+  },
+  {
+    id: 'e6971061-5f11-4dae-ab3b-3f02d524e203',
+    title: 'Rape Me (Baise-moi)',
+    year: 2000,
+    cover: 'http://dummyimage.com/186x239.jpg/5fa2dd/ffffff',
+    description:
+      'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.\n\nVestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.',
+    duration: 65,
+    contentRating: 'PG',
+    source:
+      'https://bing.com/nibh/in/lectus/pellentesque.js?velit=cubilia&id=curae&pretium=donec&iaculis=pharetra&diam=magna&erat=vestibulum&fermentum=aliquet&justo=ultrices&nec=erat&condimentum=tortor&neque=sollicitudin&sapien=mi&placerat=sit&ante=amet&nulla=lobortis&justo=sapien&aliquam=sapien&quis=non&turpis=mi&eget=integer&elit=ac&sodales=neque&scelerisque=duis&mauris=bibendum&sit=morbi&amet=non&eros=quam&suspendisse=nec&accumsan=dui&tortor=luctus&quis=rutrum&turpis=nulla&sed=tellus&ante=in&vivamus=sagittis&tortor=dui&duis=vel&mattis=nisl&egestas=duis&metus=ac&aenean=nibh&fermentum=fusce&donec=lacus&ut=purus&mauris=aliquet&eget=at&massa=feugiat&tempor=non&convallis=pretium&nulla=quis&neque=lectus&libero=suspendisse&convallis=potenti&eget=in&eleifend=eleifend&luctus=quam',
+    tags: ['Crime|Drama']
+  },
+  {
+    id: 'd920dad2-cc1b-42bd-8e2d-f8e532665905',
+    title:
+      "Danse: The Paris Opera Ballet, La (La Danse - Le ballet de l'Opéra de Paris)",
+    year: 1996,
+    cover: 'http://dummyimage.com/181x165.jpg/dddddd/000000',
+    description:
+      'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.',
+    duration: 47,
+    contentRating: 'NC-17',
+    source:
+      'https://imgur.com/cursus.html?amet=viverra&eros=eget&suspendisse=congue&accumsan=eget&tortor=semper&quis=rutrum&turpis=nulla&sed=nunc&ante=purus&vivamus=phasellus&tortor=in&duis=felis&mattis=donec&egestas=semper&metus=sapien&aenean=a&fermentum=libero&donec=nam&ut=dui',
+    tags: ['Comedy|Drama|Romance', 'Comedy|Romance', 'Drama', 'Comedy']
+  }
+];
+
+module.exports = {
+    moviesMock
+}
