922ddcc9157f7c90723426306da7502293d7a8b1
= <stephen.catano@icloud.com>
2019-11-08 00:26:15 -0500

    Creando test para nuestros endpoints

---
+++ test/routes.movies.test.js
@@ -0,0 +1,29 @@
+const assert = require('assert')
+const proxyquire = require('proxyquire')
+
+const { moviesMock, MovieServiceMock } = require('../utils/mocks/movies')
+const testServer = require('../utils/testServer')
+
+describe('routes - movies', function() {
+    const route = proxyquire('../routes/movies.js', {
+        '../services/movies.js': MovieServiceMock
+    })
+
+    const request = testServer(route)
+    describe('GET /movies', function() {
+        it ('should respond with status 200', function(done) {
+            request.get('/api/movies/').expect(200, done)
+        })
+
+        it ('should respond with the list of movies', function(done) {
+            request.get('/api/movies/').end((err, res) => {
+                assert.deepEqual(res.body, {
+                    data: moviesMock,
+                    message: 'movies listed'
+                })
+
+                done()
+            })
+        })
+    })
+})
